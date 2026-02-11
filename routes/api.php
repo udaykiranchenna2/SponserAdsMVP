93d3bc6fedc1990b573e4ac3f1932503e493d330
udaykiranchenna2 <inspizyprivatelimited@gmail.com>
2026-02-11 13:07:24 +0530

    refactor: improve API route readability by using imported controller classes.

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\BannerEmbedController;
+use App\Http\Controllers\Api\PlacementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('banners')->name('api.banners.')->group(function () {
-    Route::get('{uuid}/embed', [\App\Http\Controllers\Api\BannerEmbedController::class, 'embed'])->name('embed');
-    Route::get('{uuid}', [\App\Http\Controllers\Api\BannerEmbedController::class, 'show'])->name('show');
+    Route::get('{uuid}/embed', [BannerEmbedController::class, 'embed'])->name('embed');
+    Route::get('{uuid}', [BannerEmbedController::class, 'show'])->name('show');
 });
 
 // Placement-based banner API
 Route::prefix('placements')->name('api.placements.')->group(function () {
-    Route::post('banners', [\App\Http\Controllers\Api\PlacementController::class, 'getBanners'])->name('banners');
-    Route::post('track/impression', [\App\Http\Controllers\Api\PlacementController::class, 'trackImpression'])->name('track.impression');
-    Route::post('track/click', [\App\Http\Controllers\Api\PlacementController::class, 'trackClick'])->name('track.click');
+    Route::post('banners', [PlacementController::class, 'getBanners'])->name('banners');
+    Route::post('track/impression', [PlacementController::class, 'trackImpression'])->name('track.impression');
+    Route::post('track/click', [PlacementController::class, 'trackClick'])->name('track.click');
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
